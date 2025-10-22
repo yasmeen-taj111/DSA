44b900ebf75167d32ba0ab229bc35d0c8bb74082
yasmeen-taj111 <yasmeentaj.6041@gmail.com>
2025-10-22 14:24:09 +0530

    LinkedList basics & array to LinkedList conversion

---
+++ LinkedList/basics.java
@@ -0,0 +1,34 @@
+class Node {
+  int data;
+  Node next;
+
+  public Node(int data, Node next) {
+    this.data = data;
+    this.next = next;
+  }
+
+  public Node(int data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+public class basics {
+  private static Node array2LL(int[] a) {
+    Node head = new Node(a[0]);
+    Node nextptr = head;
+    for (int i = 1; i < a.length; i++) {
+      Node temp = new Node(a[i]);
+      nextptr.next = temp;
+      nextptr = temp;
+    }
+    return head;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5, 6 };
+    Node head = array2LL(arr);
+    System.out.println(head.data);
+  }
+
+}
