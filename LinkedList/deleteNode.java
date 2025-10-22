358bb49a6041422ff60c2dab85671aa197e53a48
yasmeen-taj111 <yasmeentaj.6041@gmail.com>
2025-10-22 16:46:12 +0530

    Deleting node in  LinkedList

---
+++ LinkedList/deleteNode.java
@@ -0,0 +1,73 @@
+// public class deleteNode {
+
+// private static Node deleteHead(Node head) {
+// if (head == null)
+// return head;
+// head = head.next;
+// return head;
+
+// }
+
+// private static Node deleteTail(Node head) {
+// if (head.next == null || head == null) {
+// return null;
+// }
+// Node temp = head;
+// while (temp.next.next != null)
+// temp = temp.next;
+
+// temp.next = null;
+
+// return head;
+// }
+
+// private static Node removeKElement(Node head, int k) {
+// if (head == null)
+// return null;
+
+// if (k == 1) {
+// head = head.next;
+// return head;
+// }
+// int c = 0;
+// Node temp = head;
+// Node prev = null;
+// while (temp != null) {
+// c++;
+// if (c == k) {
+// prev.next = prev.next.next;
+// break;
+// }
+// prev = temp;
+
+// temp = temp.next;
+
+// }
+
+// return head;
+
+// }
+
+// private static Node deleteByValue(Node head, int k) {
+// if (head == null)
+// return null;
+// if (k == head.data) {
+// head = head.next;
+// return head;
+// }
+
+// Node temp = head;
+// Node prev = null;
+// while (temp != null) {
+
+// if (temp.data == k) {
+// prev.next = prev.next.next;
+// break;
+// }
+// prev = temp;
+// temp = temp.next;
+// }
+// return head;
+// }
+
+// }
