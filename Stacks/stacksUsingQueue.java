4816784c0567e9c7d21514e095cfc0ea405a8e65
yasmeen-taj111 <yasmeentaj.6041@gmail.com>
2025-10-23 21:53:11 +0530

    Implementing stacks using Queue

---
+++ Stacks/stacksUsingQueue.java
@@ -0,0 +1,52 @@
+package Stacks;
+
+import java.util.*;
+
+public class stacksUsingQueue {
+  static Queue<Integer> q = new ArrayDeque<>();
+
+  static void push(int x) {
+    q.add(x);
+    for (int i = 1; i < q.size(); i++) {
+      q.add(q.peek());
+      q.remove();
+    }
+    System.out.println(x + " is pushed to the stack");
+  }
+
+  static void pop() {
+    if (q.isEmpty()) {
+      System.out.println("stack is empty");
+
+    }
+    System.out.println(q.peek() + " is popped from stack");
+    q.remove();
+  }
+
+  static int top() {
+    if (q.isEmpty()) {
+      System.out.println("stack is empty");
+      return -1;
+    }
+    return q.peek();
+  }
+
+  static int size() {
+    return q.size();
+  }
+
+  public static void main(String[] args) {
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    int to = top();
+    System.out.println(to + " is the topmost element of stack");
+    pop();
+    pop();
+    int s = size();
+    System.out.println(s + " is the size of stack");
+
+  }
+
+}
