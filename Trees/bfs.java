6ff887d39262b077bd207e7825f84746812c213f
yasmeen-taj111 <yasmeentaj.6041@gmail.com>
2025-12-28 01:15:06 +0530

    trees bfs

---
+++ Trees/bfs.java
@@ -0,0 +1,56 @@
+package Trees;
+
+import java.util.*;
+
+public class bfs {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    int n = scanner.nextInt();
+
+    ArrayList<Integer>[] g = new ArrayList[n + 1];
+    for (int i = 1; i <= n; i++) {
+      g[i] = new ArrayList<>();
+    }
+
+    for (int i = 1; i <= n - 1; i++) {
+      int x = scanner.nextInt();
+      int y = scanner.nextInt();
+      g[x].add(y);
+      g[y].add(x);
+    }
+
+    Queue<Integer> q = new LinkedList<>();
+    int[] used = new int[n + 1];
+
+    used[1] = 1;
+    q.offer(1);
+
+    ArrayList<Integer> leaves = new ArrayList<>();
+
+    while (!q.isEmpty()) {
+      int node = q.poll();
+      int children = 0;
+
+      for (int u : g[node]) {
+        if (used[u] == 0) {
+          used[u] = 1;
+          q.offer(u);
+          children++;
+        }
+      }
+
+      System.out.println(node + " " + children);
+
+      if (children == 0) {
+        leaves.add(node);
+      }
+    }
+
+    System.out.println("Leaves:");
+    for (int leaf : leaves) {
+      System.out.print(leaf + " ");
+    }
+  }
+}
