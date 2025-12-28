c7b683dbb7cc8de99398ab53d0d6aeaad99d8490
Yasmeen Taj <yasmeentaj.6041@gmail.com>
2025-12-28 14:05:37 +0530

    Create reverseLevelOrder.java

---
+++ Trees/reverseLevelOrder.java
@@ -0,0 +1,37 @@
+// 107. Binary Tree Level Order Traversal II
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+         List<List<Integer>> res=new ArrayList<>();
+         if(root==null) return res;
+         Queue<TreeNode> q=new LinkedList<>();
+         q.offer(root);
+         while(!q.isEmpty()){
+            int size=q.size();
+            ArrayList<Integer> level=new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node=q.poll();
+                level.add(node.val);
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
+            }
+            res.add(level);
+         }
+          Collections.reverse(res);
+         return res;
+    }
+}
